f69ff0f391dc8039da2eb435236d3ad76466e4cc
Elizabeth Jennifer Myers <elizabeth@sporksirc.net>
2011-07-04 19:29:10 -0400

    Clean up indentation.

--- src/me/cmesh/DreamLand/DreamLandEntityListener.java
+++ src/me/cmesh/DreamLand/DreamLandEntityListener.java
@@ -6,20 +6,20 @@ import org.bukkit.entity.Player;
 
 public class DreamLandEntityListener extends EntityListener
 {
-    public static DreamLand plugin;
-    public DreamLandEntityListener(DreamLand instance)
-    {
-        plugin = instance;
-    }
+	public static DreamLand plugin;
+	public DreamLandEntityListener(DreamLand instance)
+	{
+		plugin = instance;
+	}
 
-    public void onEntityDamage(EntityDamageEvent event)
-    {
-    	if (event.getEntity() instanceof Player)
-    	{
-    		if(event.getEntity().getWorld() ==  plugin.getServer().getWorld(plugin.getServer().getWorlds().get(0).getName()+"_skylands") && plugin.dreamInvincible)
-    		{
-    				event.setCancelled(true);
+	public void onEntityDamage(EntityDamageEvent event)
+	{
+		if (event.getEntity() instanceof Player)
+		{
+			if(event.getEntity().getWorld() ==  plugin.getServer().getWorld(plugin.getServer().getWorlds().get(0).getName()+"_skylands") && plugin.dreamInvincible)
+			{
+				event.setCancelled(true);
+	    		}
     		}
-    	}
-    }
+	}
 }
